163ee608223c3d8cb29eef8036b029a8e7566eb4
MinD <minhduc04022003@gmail.com>
2024-12-06 14:09:49 +0700

    add server

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,5 @@
-const API_DOMAIN = 'http://localhost:3002/';
-// const API_DOMAIN = 'https://dbjson-server.onrender.com/';
+// const API_DOMAIN = 'http://localhost:3002/';
+const API_DOMAIN = 'https://dbjson-server.onrender.com/';
 
 export const get = async (path) => {
   const response = await fetch(API_DOMAIN + path);
